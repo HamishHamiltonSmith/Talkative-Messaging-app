c782a4bcd787a5c838fec43e9f2ecbc9cb0e7df9
Hamish Hamilton-Smith <83606175+HamishHamiltonSmith@users.noreply.github.com>
2022-02-09 12:16:07 +0000

    Added server build file

--- build/server.py
+++ build/server.py
@@ -1,1 +1,30 @@
+import os
+
+
+#Set up directory structure
+
+print("[+] Ready to set up directorys, any existing talkative infuastructure shall be overwritten...")
+x = str(input("Press enter to begin: "))
+
+paths = ["/usr/share/Talkative","/usr/share/Talkative/Server/Users","/usr/share/Talkative/Server/Users/DefaultUser1","/usr/share/Talkative/Server/Chats","/usr/share/Talkative/Server/Src"]
+
+for p in paths:
+    if os.path.exists(p):
+        print(f"[+] Found existing directory: {path}, overwriting")
+        os.system(f"rm -r {p}")
+        os.system(f"mkdir {p}")
+    else:
+        os.system(f"mkdir {p}")
+
+
+#Set up directory structure
+
+print("[+] Completed directory setup, ready to begin file setup")
+x = str(input("Press enter to begin: "))
+
+
+os.system("mv ~/Downloads/Talkative/Src/Server/* /usr/share/Talkative/Server/Src")
+os.system("touch /usr/share/Talkative/Src/Server/Users/DefaultUser1/user_chats.txt")
+os.system("echo DefaultUser1>/usr/share/Talkative/Src/Server/Users/DefaultUser1/user_chats.txt")
+
 
