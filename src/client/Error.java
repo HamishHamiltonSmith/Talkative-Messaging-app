f3dddc9fd361ebc30543abeaa5963d1ec4639207
Hamish Hamilton-Smith <83606175+HamishHamiltonSmith@users.noreply.github.com>
2022-03-03 19:30:08 +0000

    Delete Error.java

--- src/client/Error.java
+++
@@ -1,9 +0,0 @@
-import javax.swing.*;
-
-public class Error {
-    
-    public void ShowError(String msg){
-        JFrame f = new JFrame("Error!");
-        JOptionPane.showMessageDialog(f, msg);
-    }
-}
